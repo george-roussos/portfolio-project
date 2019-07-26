da9ee96c36d5a91ee326917546b113f064072c20
George <georroussos@gmail.com>
2019-07-26 16:17:37 +0200

    cleanup

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 import operator
 import spacy
-from collections import Counter
-import string
 from spacy import displacy
+from collections import Counter
+from django.shortcuts import render
 
 
 def home(request):
@@ -17,17 +16,17 @@ def count(request):
 	fulltext = request.GET['fulltext']
 	nlp = spacy.load("en_core_web_sm")
 	wordlist = nlp(fulltext)
-	words = [token for token in wordlist if token.pos_ != "PUNCT" and token.pos_ != "SYM"]
-	foolproof = fulltext.split()
-	mylist = [(token.text, token.pos_) for token in wordlist if token.pos_ != 'PUNCT' and token.pos_ != "SYM"]
-	word_freq = len(words)
+	pos_tagging = [(token.text, token.pos_) for token in wordlist if token.pos_ != 'PUNCT' and token.pos_ != "SYM"]
+	word_freq = len(pos_tagging)
+	tokenized = [token.text for token in wordlist if token.pos_ != "PUNCT" and token.pos_ != "SYM"]
 	sentence_spans = list(wordlist.sents)
-	options = {'compact': True, 'page': True, 'minify': True}
+	options = {'compact': True, 'page': True, 'minify': True, 'collapse_punct': True,
+	 'collapse_phrases': True, 'offset_x':60}
 	parsing_tree = displacy.render(sentence_spans, style="dep", options=options)
-	
+	named_entities = displacy.render(sentence_spans, style="ent")
 	histogram = dict()
 
-	for word in foolproof:
+	for word in tokenized:
 		if word not in histogram:
 			histogram[word] = 1
 		else:
@@ -35,4 +34,4 @@ def count(request):
 
 	sortedwords = sorted(histogram.items(), key=operator.itemgetter(1), reverse=True)
 
-	return render(request, 'count.html',{'fulltext':fulltext,'count':word_freq,'sortedwords':sortedwords,'pos_tagging':mylist, "tree":parsing_tree})
+	return render(request, 'count.html',{'fulltext':fulltext,'count':word_freq,'sortedwords':sortedwords,'pos_tagging':pos_tagging, "tree":parsing_tree, "named_entities":named_entities})
